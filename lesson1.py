0228bc1e6f43e85e5da12b2cb87f974fe52805c7
Alexey Peresypkin <aepre3@gmail.com>
2022-02-09 07:26:38 +0300

    Add telegram_bot

--- lesson1.py
+++ lesson1.py
@@ -1,36 +1,52 @@
 import os
-
+import telegram
 import requests
 import time
 from dotenv import load_dotenv
 
-load_dotenv()
+
+def send_message(chat_id, token, response):
+    lesson_title = response.json()['new_attempts']['lesson_title']
+    is_negative = response.json()['new_attempts']['is_negative']
+    if is_negative:
+        message = f'У вас проверили работу "{lesson_title}"\n' \
+                  f'К сожалению, в работе нашлись ошибки'
+    else:
+        message = f'У вас проверили работу "{lesson_title}"\n' \
+                  f'Преподователю всё понравилось, можно приступать к следующему уроку'
+    bot = telegram.Bot(token=token)
+    bot.send_message(chat_id=chat_id, text=message)
 
 
-def req(url, headers, timestamp=None):
-    response = requests.get(url, headers=headers, params=timestamp)
-    return response
+def main():
+    load_dotenv()
+    url = 'https://dvmn.org/api/long_polling/'
+    headers = {'Authorization': os.getenv('DEVMAN_TOKEN')}
+    params = {}
+    telegram_token = os.getenv('TELEGRAM_TOKEN')
+    chat_id = os.getenv('CHAT_ID')
+    while True:
+        try:
+            response = requests.get(url, headers=headers, params=params)
+            if response.json()['status'] == 'timeout':
+                params['timestamp_to_request'] = int(
+                    response.json()['timestamp_to_request']
+                )
+            elif response.json()['status'] == 'found':
+                params['timestamp_to_request'] = int(
+                    response.json()['new_attempts']['timestamp']
+                )
+                send_message(chat_id, telegram_token, response)
+        except requests.exceptions.ReadTimeout:
+            print('Timeout')
+            time.sleep(30)
+        except requests.exceptions.ConnectionError:
+            print('Connection error')
+            time.sleep(30)
 
 
-params = {
-    'timestamp_to_request': None
-}
-url = 'https://dvmn.org/api/long_polling/'
-headers = {'Authorization': os.getenv('TOKEN')}
-
-while True:
-    try:
-        response = requests.get(url, headers=headers, params=params)
-        json_text = response.json()
-        if json_text.get('status') == 'timeout':
-            params['timestamp_to_request'] = int(json_text['timestamp_to_request'])
-            print(params['timestamp_to_request'])
-    except requests.exceptions.ReadTimeout:
-        print('Timeout')
-        time.sleep(30)
-    except requests.exceptions.ConnectionError:
-        print('Connection error')
-        time.sleep(30)
+if __name__ == '__main__':
+    main()
 
 d = {
     "request_query": [],
